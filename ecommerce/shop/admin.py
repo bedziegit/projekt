25642c3e54fc766a1c408d682bcc62c809fced5b
norbert <nkaminski1@st.swps.edu.pl>
2025-01-17 17:08:42 +0100

    lab1

--- ecommerce/shop/admin.py
+++ ecommerce/shop/admin.py
@@ -5,8 +5,11 @@ from .models import Customer, Category, Order, OrderProducts, Employee, Product
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'price', 'description',]
 
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'firstname', 'lastname', 'role',]
+
 admin.site.register(Customer)
-admin.site.register(Employee)
+admin.site.register(Employee, ProductAdmin)
 admin.site.register(Category)
 admin.site.register(Order)
 admin.site.register(OrderProducts)
